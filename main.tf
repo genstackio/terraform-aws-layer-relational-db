3c3bb64cf739b002472ee65266ca9569502d697a
Olivier Hoareau <oha@greenberets.io>
2022-07-08 15:50:27 +0200

    feat: add readonly url

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "db" {
   source                     = "genstackio/rds-cluster/aws"
-  version                    = "0.4.0"
+  version                    = "0.4.2"
   env                        = var.env
   name                       = var.name
   db_name                    = null == var.db_name ? "${var.env}_${var.name}" : var.db_name
--- outputs.tf
+++ outputs.tf
@@ -7,11 +7,17 @@ output "db_password" {
 output "db_host" {
   value = module.db.db_host
 }
+output "db_host_ro" {
+  value = module.db.db_host_ro
+}
 output "db_port" {
   value = module.db.db_port
 }
 output "db_url" {
   value = module.db.db_url
+}
+output "db_url_ro" {
+  value = module.db.db_url_ro
 }
 output "db_name" {
   value = module.db.db_name
